f52c35b234b1e7887ed1b2fec68af57fddb82c2f
Syed Ibrahim Ali <sh06565@st.habib.edu.pk>
2023-04-26 21:21:06 +0500

    updated input

--- dataset.py
+++ dataset.py
@@ -1,14 +1,19 @@
-import csv
+import pandas as pd
 
-# open the file in read mode
-with open('companies_sorted.csv', newline='') as csvfile:
-    # create a CSV reader object
-    reader = csv.reader(csvfile)
+# read the dataset
+df = pd.read_csv('companies_sorted.csv', nrows=0)
 
-    # read the first row of the file, which should contain the column names
-    column_names = next(reader)
+# get all the column names/attributes
+attributes = df.columns.tolist()[1:]
 
-# print the column names
+# print the attributes
 print("List of attributes:")
-for attribute in column_names:
-    print(attribute)
+for i, attribute in enumerate(attributes):
+    print(f"{i+1}. {attribute}")
+
+# prompt the user to select an attribute
+selected_attribute_index = int(input("Enter the index of the attribute you want to select: "))
+selected_attribute = attributes[selected_attribute_index - 1]
+
+# print the selected attribute
+print(f"You have selected '{selected_attribute}' as the attribute to search.")
